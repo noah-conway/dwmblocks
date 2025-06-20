d53848f2e516b1913d7b77c9f91165499b0e5301
noah-conway <nconway@posteo.net>
2025-06-19 19:20:34 -0700

    added update script

--- blocks.h
+++ blocks.h
@@ -2,10 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"", "date '+%A, %B %d'",				86400,  		0},
-	{" ", "/home/noah/.config/dwmblocks/scripts/cpu.sh",				    	10,		0},
+//	{" ", "/home/noah/.config/dwmblocks/scripts/cpu.sh",				    	10,		0},
 	{"", "/home/noah/.config/dwmblocks/scripts/cpu-temp.sh",				    	10,		0},
 	{"󱡠  ", "/home/noah/.config/dwmblocks/scripts/mem.sh",				    	10,		0},
 	{";", "",	0,		0},
+  {"", "/home/noah/.config/dwmblocks/scripts/updates.sh", 3600, 55},
   {"", "/home/noah/.config/dwmblocks/scripts/netcon.sh",  10, 0},
 //	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"", "/home/noah/.config/dwmblocks/scripts/clock.sh",				    	60,		0},
